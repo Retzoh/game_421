f23afb2700cf4553d23abefc86f6010ce9d24739
hbourdeau <henri.bourdeau@polyconseil.fr>
2019-09-22 22:23:00 +0200

    [UI] set viewport in page meta

--- worker/index.js
+++ worker/index.js
@@ -17,6 +17,7 @@ const html = '<!DOCTYPE html>' +
 '<html>' +
     '<head>' +
       '<meta charset="UTF-8">' +
+      '    <meta name="viewport" content="width=device-width, user-scalable=no">' +
       '<title>Main</title>' +
     '</head>' +
 '' +
